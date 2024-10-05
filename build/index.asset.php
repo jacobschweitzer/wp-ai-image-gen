0ac0727ef67677f63e35b0f60dd28254f3e35416
jacobschweitzer <3016425+jacobschweitzer@users.noreply.github.com>
2024-10-05 16:50:54 -0500

    Fix button appearing in the editor

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-rich-text'), 'version' => '25c5dfcdd556d2aa881c');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-rich-text'), 'version' => '64c947491f79ca926f90');
