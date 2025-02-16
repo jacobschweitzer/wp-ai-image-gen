85429ae5b01fa4c2d4ba23c62d16b198835af48d
jacobschweitzer <3016425+jacobschweitzer@users.noreply.github.com>
2025-02-16 11:45:32 -0600

    Fix Generate button style

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-rich-text'), 'version' => '3742971950d1fe3f5c8e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-rich-text'), 'version' => 'd01cea82fe05c63914ec');
